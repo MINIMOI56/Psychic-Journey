3af00a2be14049bd8c1b94f6c9c90d634e370ff2
MINIMOI56 <maxboucher74@hotmail.ca>
2024-05-13 02:09:34 -0400

    First Tileset and quality of life improvement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,34 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] public GameObject player;
+    [SerializeField] private GameObject player;
+    [SerializeField] private int size;
+    [SerializeField] private float maxCameraHeight;
 
     [Header("Collisions")]
-    [SerializeField] public GameObject Bottom;
-    [SerializeField] public GameObject Top;
+    private GameObject bottom;
+    private GameObject top;
+
+    void Start()
+    {
+        Camera.main.orthographicSize = size;
+
+        Vector3 topPosition = transform.position + new Vector3(0f, Camera.main.orthographicSize, 0f);
+        Vector3 bottomPosition = transform.position - new Vector3(0f, Camera.main.orthographicSize, 0f);
+
+        top = Instantiate(new GameObject(), topPosition, Quaternion.identity);
+        bottom = Instantiate(new GameObject(), bottomPosition, Quaternion.identity);
+
+        top.transform.SetParent(transform);
+        bottom.transform.SetParent(transform);
+    }
 
     void Update()
     {
-        playerPosCheck();
+        PlayerPosCheck();
     }
 
-    void playerPosCheck()
+    void PlayerPosCheck()
     {
-        if (player.transform.position.y > Top.transform.position.y)
+        if (player.transform.position.y > top.transform.position.y)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y + 16, transform.position.z);
-            Debug.Log("Top:" + Top.transform.position.y);
-            Debug.Log("Bottom:" + Bottom.transform.position.y);
+            transform.position = new Vector3(transform.position.x, transform.position.y + size * 2, transform.position.z);
         }
 
-        if (player.transform.position.y < Bottom.transform.position.y)
+        if (player.transform.position.y < bottom.transform.position.y)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y - 16, transform.position.z);
-            Debug.Log("Top:" + Top.transform.position.y);
-            Debug.Log("Bottom:" + Bottom.transform.position.y);
+            transform.position = new Vector3(transform.position.x, transform.position.y - size * 2, transform.position.z);
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,39 +7,74 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Vector3 lastVelocity;
+    private SpriteRenderer spriteRenderer;
+    private Vector3 rightGroundRaycastOffset = new Vector3(0.77f, 0f, 0f);
+    private Vector3 leftGroundRaycastOffset = new Vector3(-0.77f, 0f, 0f);
+    private Vector3 facingBlockRaycastOffset = new Vector3(0f, -0.2f, 0f);
+    private float horizontal;
+    private bool isFacingRight = true;
+    private bool isFacingBlock = false;
 
     [Header("Player Movement")]
     [SerializeField] public float speed;
     [SerializeField] public float bounceMultiplier;
+    [SerializeField] public float maxVelocity;
 
     //Debug section
     [Header("Debug section")]
     [SerializeField] public float jumpCharge;
     [SerializeField] public bool isGrounded;
-    [SerializeField] public float rayCastDistance;
+    [SerializeField] public float rayCastDistanceGround;
+    [SerializeField] public float rayCastDistanceFacingBlock;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
     }
 
     void Update()
     {
         lastVelocity = rb.velocity;
 
-        GroundCheck();
         PlayerMovements();
+        GroundCheck();
+        Flip();
+        facingBlockCheck();
+    }
+
+    void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
     }
 
     void GroundCheck()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, rayCastDistance);
-        isGrounded = hit.collider != null && hit.collider.tag == "Ground";
+        RaycastHit2D hitMiddle = Physics2D.Raycast(transform.position, Vector2.down, rayCastDistanceGround);
+        bool groundedMiddle = hitMiddle.collider != null && hitMiddle.collider.tag == "Ground";
+
+        RaycastHit2D hitRight = Physics2D.Raycast(transform.position + rightGroundRaycastOffset, Vector2.down, rayCastDistanceGround);
+        bool groundedRight = hitRight.collider != null && hitRight.collider.tag == "Ground";
+
+        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position + leftGroundRaycastOffset, Vector2.down, rayCastDistanceGround);
+        bool groundedLeft = hitLeft.collider != null && hitLeft.collider.tag == "Ground";
+
+        isGrounded = groundedMiddle || groundedRight || groundedLeft;
     }
 
     void PlayerMovements()
     {
-        float horizontal = Input.GetAxis("Horizontal");
+        horizontal = Input.GetAxisRaw("Horizontal");
 
         if (!isGrounded)
         {
@@ -51,7 +86,16 @@ public class Player : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Space) || jumpCharge >= 20)
         {
             jumpCharge = MathF.Min(jumpCharge, 20);
-            rb.velocity = new Vector2(horizontal * speed, MathF.Floor(jumpCharge));
+
+            if (isFacingBlock)
+            {
+                rb.velocity = new Vector2(-horizontal / 1.3f * speed, MathF.Floor(jumpCharge));
+            }
+            else
+            {
+                rb.velocity = new Vector2(horizontal * speed, MathF.Floor(jumpCharge));
+            }
+
             isGrounded = false;
         }
         else if (Input.GetKey(KeyCode.Space))
@@ -68,18 +112,33 @@ public class Player : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D coll)
     {
+        GroundCheck();
+
         if (!isGrounded)
         {
             float currentVelocity = lastVelocity.magnitude;
-            var direction = Vector3.Reflect(lastVelocity.normalized, coll.contacts[0].normal);
+            Vector3 direction = Vector3.Reflect(lastVelocity.normalized, coll.contacts[0].normal);
 
             rb.velocity = direction * (currentVelocity * bounceMultiplier);
         }
     }
 
+    bool facingBlockCheck()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + facingBlockRaycastOffset, isFacingRight ? Vector2.right : Vector2.left, rayCastDistanceFacingBlock);
+        isFacingBlock = hit.collider != null && hit.collider.tag == "Ground";
+        return isFacingBlock;
+    }
+
+
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * rayCastDistance);
+        Gizmos.DrawRay(transform.position, Vector2.down * rayCastDistanceGround);
+        Gizmos.DrawRay(transform.position + rightGroundRaycastOffset, Vector2.down * rayCastDistanceGround);
+        Gizmos.DrawRay(transform.position + leftGroundRaycastOffset, Vector2.down * rayCastDistanceGround);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position + facingBlockRaycastOffset, (isFacingRight ? Vector3.right : Vector3.left) * rayCastDistanceFacingBlock);
     }
 }
